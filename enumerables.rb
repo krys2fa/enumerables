fca17f7d760cffc1570f9bb5bb37bedb36e18a4b
Christopher Amanor <kriskit7@gmail.com>
2020-05-12 23:05:38 +0000

    Update methods in enumerables.rb per changes suggested in review

--- enumerables.rb
+++ enumerables.rb
@@ -1,7 +1,7 @@
 module Enumerable
   # rubocop:disable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity, Style/For, Lint/AmbiguousBlockAssociation
   def my_each
-    return unless block_given?
+    return to_enum unless block_given?
 
     for element in self
       yield(element)
@@ -9,7 +9,7 @@ module Enumerable
   end
 
   def my_each_with_index
-    return unless block_given?
+    return to_enum unless block_given?
 
     index = 0
     for element in self
@@ -19,7 +19,7 @@ module Enumerable
   end
 
   def my_select
-    return unless block_given?
+    return to_enum unless block_given?
 
     if is_a? Array
       output = []
@@ -40,6 +40,8 @@ module Enumerable
       my_each { |element| falsey += 1 unless element.is_a? arg }
     elsif !arg.nil? && arg.class == Regexp
       my_each { |element| falsey += 1 unless element.match(arg) }
+    elsif !arg.nil?
+      my_each { |element| falsey += 1 if arg != element}
     else
       my_each { |element| falsey += 1 if element == false || element.nil? }
     end
@@ -51,13 +53,15 @@ module Enumerable
     return false if empty?
 
     if block_given?
-      my_each { |element| truthy += 1 if yield(element) != false || !yield(element).nil? }
+      my_each { |element| truthy += 1 if yield(element) == true }
     elsif !arg.nil? && arg.class == Class
       my_each { |element| truthy += 1 if element.is_a? arg }
     elsif !arg.nil? && arg.class == Regexp
       my_each { |element| truthy += 1 if element.match(arg) }
+    elsif !arg.nil?
+      my_each { |element| truthy += 1 if arg == element}
     else
-      my_each { |element| truthy += 1 if element != false || !element.nil? }
+      my_each { |element| truthy += 1 if element != false && !element.nil? }
     end
 
     truthy.positive? ? true : false
@@ -72,6 +76,8 @@ module Enumerable
       my_each { |element| truthy += 1 if element.is_a? arg }
     elsif !arg.nil? && arg.class == Regexp
       my_each { |element| truthy += 1 if element.match(arg) }
+    elsif !arg.nil?
+      my_each { |element| truthy += 1 if element == arg }
     else
       my_each { |element| truthy += 1 if element == true }
     end
@@ -94,22 +100,38 @@ module Enumerable
   def my_map
     output = []
     if block_given?
-      my_each { |element| output << element if yield(element) }
+      my_each { |element| output << yield(element) || output << proc.call(element) }
     else
-      my_each { |element| output << proc.call(element) }
+      return to_enum
     end
     output
   end
 
-  def my_inject(arg = nil)
-    if arg.is_a? Symbol
-      pp reduce { |memo, element| memo.send(arg, element) }
+  def my_inject(*args)
+    output = []
+    operator = nil
+
+    if args.length > 1
+      args.my_each do |element| 
+        if element.is_a? Symbol
+          operator = element
+        else 
+          output << element 
+        end
+      end
+      reduce { |memo, element| memo.send(operator, element) }
     else
-      memo = arg
-      memo = my_each { |element| memo.nil? ? element : yield(memo, element) } if block_given?
-      pp memo
+      if args[0].is_a? Symbol
+        reduce { |memo, element| memo.send(args[0], element) }
+      else
+        memo = args[0]
+        my_each { |element| memo.nil? ? memo = element : memo = yield(memo, element) } if block_given?
+        memo
+      end
     end
+    
   end
+
 end
 
 def multiply_els(arr)
